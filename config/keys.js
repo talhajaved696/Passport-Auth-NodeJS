2f1770a5a9db5efd6e68d1ffb03abcf62d74b219
talhajaved696 <45163430+talhajaved696@users.noreply.github.com>
2021-07-14 02:10:19 +0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://Talha:bilaljaved696@cluster0.tstxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://<username>:<password>@cluster0.tstxr.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
